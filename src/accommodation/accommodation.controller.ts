af67f544ea9da544e7940bac96f931e0301d556b
lnicepei <95967146+lnicepei@users.noreply.github.com>
2023-11-27 20:12:39 +0300

    feat/searchAndPagination (#2)

--- src/accommodation/accommodation.controller.ts
+++ src/accommodation/accommodation.controller.ts
@@ -6,10 +6,18 @@ import {
   Param,
   Patch,
   Post,
+  Query,
+  Req,
 } from '@nestjs/common';
+
+import { Request } from 'express';
+import { Pagination } from 'nestjs-typeorm-paginate';
+
+import { Observable } from 'rxjs';
 import { AccommodationService } from './accommodation.service';
 import { CreateAccommodationDto } from './dto/create-accommodation.dto';
 import { UpdateAccommodationDto } from './dto/update-accommodation.dto';
+import { Accommodation } from './entities/accommodation.entity';
 
 @Controller('accommodations')
 export class AccommodationController {
@@ -26,9 +34,25 @@ export class AccommodationController {
     return this.accommodationService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.accommodationService.findOne(+id);
+  @Get('search')
+  index(
+    @Query('page') page = 1,
+    @Query('limit') limit = 10,
+    @Query('q') q = '',
+    @Req() request: Request,
+  ): Observable<Pagination<Accommodation>> {
+    limit = limit > 100 ? 100 : limit;
+
+    return this.accommodationService.search(
+      {
+        page: Number(page),
+        limit: Number(limit),
+        route: `${request.protocol}://${request.get('Host')}${
+          request.originalUrl
+        }`,
+      },
+      q,
+    );
   }
 
   @Patch(':id')
--- src/accommodation/accommodation.service.ts
+++ src/accommodation/accommodation.service.ts
@@ -1,6 +1,8 @@
 import { HttpException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { IPaginationOptions, Pagination } from 'nestjs-typeorm-paginate';
+import { Observable, from, map } from 'rxjs';
+import { Like, Repository } from 'typeorm';
 import { CreateAccommodationDto } from './dto/create-accommodation.dto';
 import { UpdateAccommodationDto } from './dto/update-accommodation.dto';
 import { Accommodation } from './entities/accommodation.entity';
@@ -61,5 +63,54 @@ export class AccommodationService {
     }
 
     return await this.accommodationRepository.delete(id);
+  }
+
+  search(
+    options: IPaginationOptions,
+    q: string,
+  ): Observable<Pagination<Accommodation>> {
+    return from(
+      this.accommodationRepository.findAndCount({
+        skip: (Number(options.page) - 1) * Number(options.limit) || 0,
+        take: Number(options.limit) || 10,
+        select: [
+          'id',
+          'name',
+          'description',
+          'rating',
+          'price',
+          'provider',
+          'location',
+        ],
+        where: [{ name: Like(`%${q}%`) }, { description: Like(`%${q}%`) }],
+      }),
+    ).pipe(
+      map(([accommodations, totalAccommodations]) => {
+        const AccommodationsPageable: Pagination<Accommodation> = {
+          items: accommodations,
+          links: {
+            first: options.route + `?limit=${options.limit}`,
+            previous: options.route + ``,
+            next:
+              options.route +
+              `?limit=${options.limit}&page=${Number(options.page) + 1}`,
+            last:
+              options.route +
+              `?limit=${options.limit}&page=${Math.ceil(
+                totalAccommodations / Number(options.limit),
+              )}`,
+          },
+          meta: {
+            currentPage: Number(options.page),
+            itemCount: accommodations.length,
+            itemsPerPage: Number(options.limit),
+            totalItems: totalAccommodations,
+            totalPages: Math.ceil(totalAccommodations / Number(options.limit)),
+          },
+        };
+
+        return AccommodationsPageable;
+      }),
+    );
   }
 }
